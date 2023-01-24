d7da30d52aee50aabb7f7e33e52f3b665c2edc11
jyang077 <112518069+jyang077@users.noreply.github.com>
2023-01-24 18:40:53 -0500

    Utensils

--- src/main/java/net/prehistoricdiner/init/ModItems.java
+++ src/main/java/net/prehistoricdiner/init/ModItems.java
@@ -7,6 +7,11 @@ import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 public class ModItems {
     public static final Item food_item = null;
     public static final Item skillet = null;
+    public static final Item pot = null;
+    public static final Item grinder = null;
+    public static final Item rolling_pin = null;
+    public static final Item ladle = null;
+    public static final Item knife = null;
 
     public ModItems() {
     }
