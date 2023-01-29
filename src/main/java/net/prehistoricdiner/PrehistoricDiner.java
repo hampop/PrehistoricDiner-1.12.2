f7be4036b2a48873dfc3e9fd69365ae3ae5e94bc
AechtRob <aecht_rob@outlook.com>
2023-01-29 23:03:09 +0000

    Damageable crafting ingredients

--- src/main/java/net/prehistoricdiner/PrehistoricDiner.java
+++ src/main/java/net/prehistoricdiner/PrehistoricDiner.java
@@ -1,12 +1,17 @@
 package net.prehistoricdiner;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.oredict.OreDictionary;
+import net.prehistoricdiner.item.ItemDamageableCrafted;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -31,7 +36,22 @@ public class PrehistoricDiner {
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-
+        try {
+            for (Object item : Item.REGISTRY) {
+                if (item instanceof ItemDamageableCrafted) {
+                    String oreDict = ForgeRegistries.ITEMS.getKey((Item)item).toString();
+                    OreDictionary.registerOre(oreDict, new ItemStack((Item) item, 1));
+                    int i = 1;
+                    while (i <= ((ItemDamageableCrafted) item).getMaxDamage()) {
+                        ItemStack stack = new ItemStack((Item) item, 1);
+                        stack.setItemDamage(i);
+                        OreDictionary.registerOre(oreDict, stack);
+                        i ++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+        }
     }
 
     @EventHandler
--- src/main/java/net/prehistoricdiner/item/ItemDamageableCrafted.java
+++ src/main/java/net/prehistoricdiner/item/ItemDamageableCrafted.java
@@ -3,17 +3,24 @@ package net.prehistoricdiner.item;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import javax.annotation.Nonnull;
+
 public class ItemDamageableCrafted extends Item {
 
+    public ItemDamageableCrafted(int maxDamage) {
+        setContainerItem(this);
+        setMaxDamage(maxDamage);
+    }
+
     @Override
-    public ItemStack getContainerItem(ItemStack stack) {
-        ItemStack result = stack.copy();
-        result.setItemDamage(stack.getItemDamage() + 1);
+    public ItemStack getContainerItem(@Nonnull ItemStack itemStack) {
+        ItemStack result = itemStack.copy();
+        result.setItemDamage(itemStack.getItemDamage() + 1);
         return result;
     }
 
     @Override
-    public boolean hasContainerItem(ItemStack stack) {
+    public boolean hasContainerItem(@Nonnull ItemStack stack) {
         return stack.getItemDamage() < stack.getMaxDamage();
     }
 }
